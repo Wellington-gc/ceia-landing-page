5b7a1cad3231da8d9c51ecc13bdfa10c76047f87
Wellington-gc <wellington_gc@outlook.com>
2024-05-13 00:15:59 -0300

    Add footer and locale switcher

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,13 +1,11 @@
 import PageBase from "@/components/PageBase";
-import { BackgroundImage, Container } from "@mantine/core";
+import { Container } from "@mantine/core";
 
 export default function HomePage() {
     return (
         <>
             <PageBase>
-                <BackgroundImage src="/background.png" style={{ height: '100vh' }}>
-                </BackgroundImage>
-                <Container fluid h={250} bg={"blue"}></Container>
+                <Container fluid h={250} bg={"white"}></Container>
             </PageBase>
         </>
     );
--- components/PageBase.tsx
+++ components/PageBase.tsx
@@ -1,12 +1,11 @@
 'use client';
 
-import NextImage from "next/image";
 import NavigationLink from "./NavigationLink";
 import Link from "next/link";
-import { AppShell, Burger, Flex, Group, Image } from "@mantine/core";
+import Footer from "./Footer";
+import LocaleSwitcherSelect from "./LocaleSwitcherSelect";
+import { AppShell, BackgroundImage, Burger, Container, Flex, Group, Image, Stack } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebook, faInstagram, faLinkedin, faYoutube } from "@fortawesome/free-brands-svg-icons";
 import { useLocale, useTranslations } from "next-intl";
 
 type Props = {
@@ -21,20 +20,19 @@ export default function PageBase({ children }: Props) {
     return (
         <>
             <AppShell
-                header={{ height: 60 }}
-                navbar={{ width: 300, breakpoint: 'sm', collapsed: { desktop: true, mobile: !opened } }}
-                padding="md"
-            >
-                <AppShell.Header withBorder={false} bg={"transparent"} c={"white"}>
+                header={{ height: 0 }}
+                navbar={{ width: 300, breakpoint: 'md', collapsed: { desktop: true, mobile: !opened } }}
+                padding="md">
+                <AppShell.Header withBorder={false} bg={"transparent"} c={"white"} pos={"absolute"}>
                     <Group h="100%" p="xl" justify="space-between">
-                        <Group>
-                            <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" color="white" />
+                        <Group gap={"xl"}>
+                            <Burger opened={opened} onClick={toggle} hiddenFrom="md" size="md" color="white" />
                             <Link href={`/${locale}`}>
-                                <Image component={NextImage} src={'/logo.png'} alt="Logo" width={57} height={73} priority={true} />
+                                <Image src={'/logo.png'} alt="Logo" w={57} h={73} />
                             </Link>
                         </Group>
                         <Group>
-                            <Flex visibleFrom="sm" gap="3.6rem">
+                            <Flex visibleFrom="md" gap="3.6rem">
                                 <NavigationLink href={'/about'}>{t('about')}</NavigationLink>
                                 <NavigationLink href={'/solutions'}>{t('solutions')}</NavigationLink>
                                 <NavigationLink href={'/highlights'}>{t('highlights')}</NavigationLink>
@@ -42,33 +40,36 @@ export default function PageBase({ children }: Props) {
                                 <NavigationLink href={'/human-resources'}>{t('human-resources')}</NavigationLink>
                                 <NavigationLink href={'/embrapii'}>EMBRAPII</NavigationLink>
                             </Flex>
+                            <LocaleSwitcherSelect />
                         </Group>
                     </Group>
                 </AppShell.Header>
 
-                <AppShell.Navbar py="md" px={4} h={"300px"} mt={50}>
-                    <NavigationLink href={'/about'}>{t('about')}</NavigationLink>
-                    <NavigationLink href={'/solutions'}>{t('solutions')}</NavigationLink>
-                    <NavigationLink href={'/highlights'}>{t('highlights')}</NavigationLink>
-                    <NavigationLink href={'/contact'}>{t('contact')}</NavigationLink>
-                    <NavigationLink href={'/human-resources'}>{t('human-resources')}</NavigationLink>
-                    <NavigationLink href={'/embrapii'}>EMBRAPII</NavigationLink>
+                <AppShell.Navbar c={"white"} bg={"black"} withBorder={false} pos={"absolute"}>
+                    <Stack m={"xl"}>
+                        <Burger opened={opened} onClick={toggle} size="md" color="white" />
+                        <NavigationLink href={'/about'}>{t('about')}</NavigationLink>
+                        <NavigationLink href={'/solutions'}>{t('solutions')}</NavigationLink>
+                        <NavigationLink href={'/highlights'}>{t('highlights')}</NavigationLink>
+                        <NavigationLink href={'/contact'}>{t('contact')}</NavigationLink>
+                        <NavigationLink href={'/human-resources'}>{t('human-resources')}</NavigationLink>
+                        <NavigationLink href={'/embrapii'}>EMBRAPII</NavigationLink>
+                    </Stack>
                 </AppShell.Navbar>
 
                 <AppShell.Main p={0} c="white">
+                    <BackgroundImage src="/background.png" style={{ height: '100vh' }} />
                     {children}
                 </AppShell.Main>
 
-                <AppShell.Footer pos={"static"} bottom={0} h={300} withBorder={false} style={{
-                    'background': 'linear-gradient(90deg, #041026 0%, #013A65 100%)',
-                    'border-radius': '20px 20px 0 0',
-                }}>
-                    <Flex>
-                        <FontAwesomeIcon icon={faFacebook} />
-                        <FontAwesomeIcon icon={faInstagram} />
-                        <FontAwesomeIcon icon={faLinkedin} />
-                        <FontAwesomeIcon icon={faYoutube} />
-                    </Flex>
+                <AppShell.Footer
+                    pos={"static"}
+                    bottom={0}
+                    mih={300}
+                    withBorder={false}
+                    bg="linear-gradient(90deg, #041026 0%, #013A65 100%)"
+                    style={{ 'borderRadius': '20px 20px 0 0' }}>
+                    <Footer />
                 </AppShell.Footer>
             </AppShell>
         </>
